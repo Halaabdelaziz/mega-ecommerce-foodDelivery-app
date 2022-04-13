e981c1ce6ef445d5862e39d96552235575c6880b
esraa898 <essomohamed121@gmail.com>
2022-04-13 21:45:40 +0200

    Route

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use App\Http\Controllers\admin\CategoryController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', function () {
+Route::get('/layout', function () {
 
     return view('layouts.master');
 });
