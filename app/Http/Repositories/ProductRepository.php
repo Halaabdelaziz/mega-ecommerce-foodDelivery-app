e20486c0b44d6004ccc4ba8f308c48473fe40b1e
esraa898 <essomohamed121@gmail.com>
2022-04-19 15:46:24 +0200

    handling api and adding resource

--- app/Http/Repositories/ProductRepository.php
+++ app/Http/Repositories/ProductRepository.php
@@ -3,9 +3,9 @@ namespace App\Http\Repositories;
 
 use App\Models\Product;
 use App\Http\Traits\ApiResponceTrait;
+use App\Http\Resources\productResource;
 use App\Http\Interfaces\ProductInterface;
-
-
+use App\Http\Resources\restaruntResource;
 
 class ProductRepository implements ProductInterface{
 
@@ -21,13 +21,13 @@ class ProductRepository implements ProductInterface{
     
     public function index(){
         
-        $products=$this->productModel::select('id','name','price')->get();
+        $products=$this->productModel::select('id','name','price','image')->get();
         return $this->apiResponce(200,'All products',null,$products);
             
     }
     public function  productDetails($id){ 
-         
-        $product = $this->productModel::where('id',$id)->with('categories:id,name','restarunt:id,name',)->get();
+        $product=  productResource::collection($this->productModel::where('id',$id)->with('category:id,name','restarunt:id,name,image')->get());
+        
        return $this->apiResponce(200,'product details',null,$product);
 
    }
